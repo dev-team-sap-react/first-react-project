2095f718f604b08c072a5cfe54d45ab61f2a8776
bburakcebeci <62771823+bburakcebeci@users.noreply.github.com>
2023-10-17 12:19:55 +0200

    added processedData variable to RawData and Evaluation pages with useContext..

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,29 +1,20 @@
-import { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import { createContext, useState } from "react";
 
 export const AppContext = createContext();
 
 
-const url = "https://apis-for-beginner.bscebeci.de/api/employees";
 
 export const AppProvider = ({ children }) => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [loadedEmployees, setLoadedEmployees] = useState([]);
+
+    const [processedData, setProcessedData] = useState([]);
   
-    useEffect(() => {
-      (async () => {
-        const response = (await axios.get(url)).data;
-        setLoadedEmployees(response);
-        setIsLoading(false);
-      })();
-    }, [loadedEmployees]);
 
   
     return (
       <AppContext.Provider
         value={{
-          isLoading,
-          loadedEmployees,
+          processedData,
+          setProcessedData
         }} >
         {children}
       </AppContext.Provider>
